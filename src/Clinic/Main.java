abc53d470f6ab279f6a94dd715f412e5595aac60
ssala71 <ssalazar7190@gmail.com>
2025-10-12 23:15:40 -0700

    Fixed pet error

--- src/Clinic/Main.java
+++ src/Clinic/Main.java
@@ -1,11 +1,5 @@
 package Clinic;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
